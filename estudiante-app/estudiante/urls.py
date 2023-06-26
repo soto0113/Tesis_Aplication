ca0b040c41f382f570dd66ee92bba7dc1e9e0798
soto0113 <83653740+soto0113@users.noreply.github.com>
2023-06-26 11:01:51 -0500

    Listo

--- estudiante-app/estudiante/urls.py
+++ estudiante-app/estudiante/urls.py
@@ -1,12 +1,15 @@
 from django.urls import path
-from .views import view_estudiante, create_estudiante, delete_estudiante, edit_estudiante
-
+from .views import view_estudiante, create_estudiante, delete_estudiante, edit_estudiante, base, login_estudiante,logout_estudiante
+from django.contrib.auth.views import LogoutView
 from django.conf import settings
 from django.contrib.staticfiles.urls import static
 
 urlpatterns = [
-    path('', view_estudiante, name="estudiantes"),
+    path('', base, name='base'),
+    path('login_estudiante', login_estudiante, name="login_estudiante"),
+    path('view_estudiante', view_estudiante, name="estudiantes"),
     path('new_estudiante/', create_estudiante, name="create_estudiante"),
     path('delete_estudiante/<int:id>', delete_estudiante, name="delete_estudiante"),
     path('edit_estudiante/<int:id>', edit_estudiante, name="edit_estudiante"),
+    path('logout/', logout_estudiante, name='logout'),
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- estudiante-app/estudiante/views.py
+++ estudiante-app/estudiante/views.py
@@ -4,12 +4,19 @@ from .forms import EstudianteForm
 from django.contrib import messages
 from django.core.files.storage import default_storage
 from django.core.files.base import ContentFile
+from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
+
 # Create your views here.
+
+def base(request):
+    return render(request, 'base.html')
 
 def view_estudiante(request):
     estudiantes = Estudiante.objects.all()
     return render(request, 'view_estudiante.html', {'estudiantes': estudiantes})
 
+@login_required
 def create_estudiante(request):
     if request.method == 'POST':
         form = EstudianteForm(request.POST, request.FILES)
@@ -20,6 +27,7 @@ def create_estudiante(request):
         form = EstudianteForm()
     return render(request, 'create_estudiante.html', {'forms': form})
 
+@login_required
 def delete_estudiante(request, id):
     estudiante = Estudiante.objects.get(id=id)
     if request.method == 'POST':
@@ -28,6 +36,7 @@ def delete_estudiante(request, id):
         return redirect('/')
     return render(request, 'delete_estudiante.html', {'estudiante': estudiante})
 
+@login_required
 def edit_estudiante(request, id):
     estudiante = Estudiante.objects.get(id=id)
     forms = EstudianteForm(request.POST or None, instance=estudiante)
@@ -36,29 +45,30 @@ def edit_estudiante(request, id):
         return redirect('/')
     return render(request, 'edit_estudiante.html', {'forms': forms})
 
+def login_estudiante(request):
+    if request.method == 'POST':
+        # Obtener el usuario y la contraseña del formulario
+        username = request.POST['username']
+        password = request.POST['password']
+        
+        # Autenticar al usuario
+        user = authenticate(request, username=username, password=password)
+        
+        # Si el usuario es autenticado correctamente, iniciar sesión y redirigir al usuario a la página de inicio
+        if user is not None:
+            login(request, user)
+            return redirect('/')
+        else:
+            # Si la autenticación falla, mostrar un mensaje de error
+            messages.error(request, 'El usuario o la contraseña son incorrectos.')
+    else:
+        # Si el método no es POST, mostrar un mensaje de información
+        messages.info(request, 'Por favor inicia sesión.')
+    
+    # Renderizar el formulario de inicio de sesión
+    return render(request, 'login_estudiante.html')
 
-# def edit_estudiante(request, id):
-#     estudiante = Estudiante.objects.get(id=id)
-#     if request.method == 'POST':
-#         forms = EstudianteForm(request.POST, request.FILES, instance=estudiante)
-#         if forms.is_valid():
-#             # Eliminar imagen anterior
-#             if estudiante.foto:
-#                 default_storage.delete(estudiante.foto.name)
-#             # Guardar nueva imagen
-#             foto = forms.cleaned_data.get('foto')
-#             if foto:
-#                 filename = f'{estudiante.codigo}/{foto.name}'
-#                 path = default_storage.save(f'imagenes/{filename}', ContentFile(foto.read()))
-#                 estudiante.foto.name = path
-#             # Guardar otros datos del formulario
-#             estudiante.nombre = forms.cleaned_data.get('nombre')
-#             estudiante.apellido = forms.cleaned_data.get('apellido')
-#             estudiante.codigo = forms.cleaned_data.get('codigo')
-#             estudiante.correo = forms.cleaned_data.get('correo')
-#             estudiante.save()
-#             messages.success(request, 'El estudiante ha sido actualizado exitosamente.')
-#             return redirect('/')
-#     else:
-#         forms = EstudianteForm(instance=estudiante)
-#     return render(request, 'edit_estudiante.html', {'forms': forms})
+@login_required
+def logout_estudiante(request):
+    logout(request)
+    return redirect('/')
