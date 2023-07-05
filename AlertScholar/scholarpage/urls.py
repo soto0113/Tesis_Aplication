c40f55157d3b4b07ee3a6bedd4ae04936662c8ba
soto0113 <83653740+soto0113@users.noreply.github.com>
2023-07-05 11:41:05 -0500

    comm

---
+++ AlertScholar/scholarpage/urls.py
@@ -0,0 +1,17 @@
+from django.urls import path
+from .views import view_estudiante, create_estudiante, delete_estudiante, edit_estudiante, base, login_estudiante,logout_estudiante, carga_excel, regresion_bosques_aleatorios
+from django.contrib.auth.views import LogoutView
+from django.conf import settings
+from django.contrib.staticfiles.urls import static
+
+urlpatterns = [
+    path('', base, name='base'),
+    path('login_estudiante', login_estudiante, name="login_estudiante"),
+    path('view_estudiante', view_estudiante, name="estudiantes"),
+    path('new_estudiante/', create_estudiante, name="create_estudiante"),
+    path('delete_estudiante/<int:id>', delete_estudiante, name="delete_estudiante"),
+    path('edit_estudiante/<int:id>', edit_estudiante, name="edit_estudiante"),
+    path('carga_excel', carga_excel, name='carga_excel'),
+    path('regresion-bosques-aleatorios/', regresion_bosques_aleatorios, name='regresion_bosques_aleatorios'),
+    path('logout/', logout_estudiante, name='logout'),
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
